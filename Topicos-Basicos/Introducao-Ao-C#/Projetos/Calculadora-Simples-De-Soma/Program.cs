d957088dc9a6dd53cb8174df2cfb5d4b04b7788d
GabrielMendes16 <gabriel1902008@hotmail.com>
2024-03-09 10:07:02 -0300

    inserindo o código do programa

---
+++ Topicos-Basicos/Introducao-Ao-C#/Projetos/Calculadora-Simples-De-Soma/Program.cs
@@ -0,0 +1,25 @@
+﻿decimal numero1 = 0;
+decimal numero2 = 0;
+Console.WriteLine("Que tal realizarmos uma soma?");
+Console.WriteLine("Informe O Primeiro Número:");
+numero1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Informe O Segundo Número:");
+numero2 = Convert.ToDecimal(Console.ReadLine());
+decimal resultado = numero1 + numero2;
+Console.WriteLine($"Resultado da soma entre {numero1} + {numero2} é = {resultado}");
+
+int primeiraEntradaDoUsuario = 0;
+int segundaEntradaDoUsuario = 0;
+
+Console.WriteLine("Primeira Entrada:");
+primeiraEntradaDoUsuario = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Segunda Entrada:");
+segundaEntradaDoUsuario = int.Parse(Console.ReadLine());
+
+
+int somaDasEntradas = primeiraEntradaDoUsuario + segundaEntradaDoUsuario;
+
+
+Console.WriteLine($"Resultado da soma entre:  {primeiraEntradaDoUsuario} + {segundaEntradaDoUsuario} = {somaDasEntradas}");
