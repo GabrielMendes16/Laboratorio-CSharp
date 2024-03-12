13dc43d814f1ae4e7ae9c57a5e1edd6f6a34cb50
GabrielMendes16 <gabriel1902008@hotmail.com>
2024-03-12 12:29:53 -0300

    Adicionado novos projetos

---
+++ Topicos-Basicos/Introducao-Ao-C#/Projetos/Conversor-De-Temperatura/Program.cs
@@ -0,0 +1,11 @@
+﻿double temperatura = 0;
+Console.WriteLine("Informe a temperatura em graus Celsius:");
+if (double.TryParse(Console.ReadLine(), out temperatura))
+{
+    double resultado = (temperatura * 1.8) + 32;
+    Console.WriteLine($"A temperatura em Fahrenheit é: {resultado:F1}");
+}
+else
+{
+    Console.WriteLine("Por favor, insira um valor numérico válido.");
+}
---
+++ Topicos-Basicos/Introducao-Ao-C#/Projetos/Idade-Futura/Program.cs
@@ -0,0 +1,10 @@
+﻿int idade = 0;
+
+Console.WriteLine("Olá, qual é sua idade?");
+idade = int.Parse(Console.ReadLine());
+idade += 10;
+Console.WriteLine($"Sabia que daqui 10 anos você terá {idade} anos?");
+
+
+
+
